7f51c3fff6704e7b9babae3fcbdbdef17f81bb48
Yaroslav Ognevoy <yognevoy@korusconsulting.ru>
2026-02-18 23:55:45 +0300

    fix(routes): change upload route to /upload

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [UploadController::class, 'index']);
+    Route::get('/upload', [UploadController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
