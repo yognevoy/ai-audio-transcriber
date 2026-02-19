40d7ccd403b3d9c77e1f525fe4f8223bfb01716b
Yaroslav Ognevoy <yognevoy@korusconsulting.ru>
2026-02-19 22:35:08 +0300

    feat: change registration link label

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -91,7 +91,7 @@ const submit = () => {
                     :href="route('register')"
                     class="ms-4 rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                    Sign up
+                    Register
                 </Link>
 
                 <PrimaryButton
