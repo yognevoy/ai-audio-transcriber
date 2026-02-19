49c926bcad0afcea5533262d662f0cd4f810affd
Yaroslav Ognevoy <yognevoy@korusconsulting.ru>
2026-02-19 22:50:58 +0300

    feat: remove dashboard page

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,7 +22,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
-                                <Link :href="route('dashboard')">
+                                <Link :href="route('upload')">
                                     <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
@@ -34,10 +34,10 @@ const showingNavigationDropdown = ref(false);
                                 class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
                             >
                                 <NavLink
-                                    :href="route('dashboard')"
-                                    :active="route().current('dashboard')"
+                                    :href="route('upload')"
+                                    :active="route().current('upload')"
                                 >
-                                    Dashboard
+                                    Upload
                                 </NavLink>
                             </div>
                         </div>
@@ -136,10 +136,10 @@ const showingNavigationDropdown = ref(false);
                 >
                     <div class="space-y-1 pb-3 pt-2">
                         <ResponsiveNavLink
-                            :href="route('dashboard')"
-                            :active="route().current('dashboard')"
+                            :href="route('upload')"
+                            :active="route().current('upload')"
                         >
-                            Dashboard
+                            Upload
                         </ResponsiveNavLink>
                     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [UploadController::class, 'index'])->name('upload');
